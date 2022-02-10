282ef667a4714cfb5894d4ea5306249e2b79faa6
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-02-09 21:30:44 -0700

    Add favorite books

---
+++ challenge/favorite_books.js
@@ -0,0 +1,17 @@
+// I named this file favoritebooks.js because it's about my favorite books!
+
+// my favorite book title
+var favoriteBook = "The Girl on the Train";
+// how many pages the book has
+var bookLength = 385;
+// I think the book is interesting
+var isBookInteresting = true;
+// The book is not a hard cover
+var isBookHardCover = false;
+// It is a suspensful book!
+var bookGenre = "Suspense";
+// My favorite author is Paula Hawkins
+var favoriteAuthor = "Paula Hawkins";
+
+// Below are book genres that I'm interested in!
+var bookGenres = ["Suspense", "Horror", "Science-Fiction", "Romance", "Thrillers"];
---
+++ challenge/index.md
@@ -0,0 +1,6 @@
+## Books
+* The Girl on the Train
+* Atomic Habits
+* Baby Teeth
+* In a Dark, Dark Wood
+* Harry Potter
